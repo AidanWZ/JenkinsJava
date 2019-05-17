9029545a1c24d8c9107069d44978f6f0eadeb689
AidanWZ <43284791+AidanWZ@users.noreply.github.com>
2019-05-17 14:58:19 -0400

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,3 +1,7 @@
+/** 
+ * The HelloWorld class simply prints "Hello World" to standard output.
+ */
+
 public class HelloWorld {
   public static void main(String[] args) {
     System.out.println("Hello, World");
