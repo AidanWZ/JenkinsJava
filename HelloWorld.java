c20e30508613e0895f32be49fe0a0ff117adb2b6
Aidan Zimmermann <aidan.zimmermann@gmail.com>
2019-05-20 14:24:20 -0400

    Delete HelloWorld.java

--- HelloWorld.java
+++
@@ -1,9 +0,0 @@
-/** 
- * The HelloWorld class simply prints "Hello World" to standard output.
- */
-
-public class HelloWorld {
-  public static void main(String[] args) {
-    System.out.println("Hello, World");
-  }
-}
